de5d1a2122f9c370d5c6663982e8df0cff15ef99
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2023-06-02 17:42:02 +0300

    Added todo for implement consuming message properties

--- template/src/Contracts/MessageContract.cs
+++ template/src/Contracts/MessageContract.cs
@@ -2,5 +2,6 @@ namespace RabbitMqConsumerTemplate.Contracts;
 
 public class MessageContract
 {
+    //TODO: implement message properties here
     public string Property { get; set; } = string.Empty;
 }
