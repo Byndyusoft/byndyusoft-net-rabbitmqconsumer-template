08bc5b048f474a996e54ee8db5d0cdd5fd1db9c2
Nikolay Korsakov <korsakov@byndyusoft.com>
2024-12-04 17:32:48 +0300

    Startup removed Switched to top-level statement nugets fixed

--- template/src/RabbitMqConsumerTemplate/Program.cs
+++ template/src/RabbitMqConsumerTemplate/Program.cs
@@ -1,49 +1,36 @@
-namespace RabbitMqConsumerTemplate;
-
 using Byndyusoft.Logging.Configuration;
 using Byndyusoft.MaskedSerialization.Serilog.Extensions;
-using Infrastructure.OpenTelemetry;
-using Microsoft.AspNetCore.Hosting;
 using OpenTelemetry.Trace;
+using RabbitMqConsumerTemplate;
+using RabbitMqConsumerTemplate.Infrastructure.OpenTelemetry;
+using RabbitMqConsumerTemplate.Installers;
 using Serilog;
 
-public static class Program
+var serviceName = typeof(Program).Assembly.GetName().Name!;
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var rabbitConnectionString = builder.Configuration.GetConnectionString("Rabbit");
+
+builder.Host.UseSerilog(ConfigureSerilogAction);
+services.AddOpenTelemetry(serviceName,
+                          builder.Configuration.GetSection("OtlpExporterOptions").Bind,
+                          _builder => _builder.AddRabbitMqClientInstrumentation(),
+                          _builder => _builder.AddTemplateMetrics());
+services.AddHostedService<Worker>();
+services.AddRabbitMqClient(options => { options.ConnectionString = rabbitConnectionString; });
+services.AddHealthChecks();
+services.AddApplicationServices();
+
+var app = builder.Build();
+app.UseHealthChecks("/healthz")
+   .UseOpenTelemetryPrometheusScrapingEndpoint();
+app.Run();
+
+static void ConfigureSerilogAction(HostBuilderContext context, LoggerConfiguration configuration)
 {
-    private static readonly string ServiceName = typeof(Program).Assembly.GetName().Name!;
-
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    private static IHostBuilder CreateHostBuilder(string[] args)
-    {
-        return Host.CreateDefaultBuilder(args)
-                   .ConfigureWebHostDefaults(ConfigureWebHostDefaultsAction)
-                   .ConfigureServices(ConfigureServicesAction)
-                   .UseSerilog(ConfigureSerilogAction);
-    }
-
-    private static void ConfigureWebHostDefaultsAction(IWebHostBuilder builder)
-    {
-        builder.UseStartup<Startup>();
-    }
-
-    private static void ConfigureServicesAction(HostBuilderContext context, IServiceCollection services)
-    {
-        services.AddOpenTelemetry(ServiceName,
-                                  context.Configuration.GetSection("OtlpExporterOptions").Bind,
-                                  builder => builder.AddRabbitMqClientInstrumentation(),
-                                  builder => builder.AddTemplateMetrics());
-        services.AddHostedService<Worker>();
-    }
-
-    private static void ConfigureSerilogAction(HostBuilderContext context, LoggerConfiguration configuration)
-    {
-        configuration
-            .UseDefaultSettings(context.Configuration)
-            .UseOpenTelemetryTraces()
-            .WriteToOpenTelemetry()
-            .WithMaskingPolicy();
-    }
+    configuration
+        .UseDefaultSettings(context.Configuration)
+        .UseOpenTelemetryTraces()
+        .WriteToOpenTelemetry()
+        .WithMaskingPolicy();
 }
